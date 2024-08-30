33c7c0a07352588392e35a0486f8449a95591a10
Restyled.io <commits@restyled.io>
2024-08-30 10:44:49 +0000

    Restyled by whitespace

--- data_subscriber/cslc/cslc_query.py
+++ data_subscriber/cslc/cslc_query.py
@@ -108,7 +108,7 @@ class CslcCmrQuery(CmrQuery):
         In forward  and re-processing modes, extend the granules with potentially additional records
         if a burst belongs to two frames.
 
-        :param granules: 
+        :param granules:
         :param no_duplicate:  (Default value = False)
         :param force_frame_id:  (Default value = None)
 
@@ -158,9 +158,9 @@ class CslcCmrQuery(CmrQuery):
         """For CSLC this is used to determine download_batch_id and attaching it the granule.
         Function extend_additional_records must have been called before this function.
 
-        :param granule: 
-        :param args: 
-        :param granule_id: 
+        :param granule:
+        :param args:
+        :param granule_id:
 
         """
 
@@ -190,7 +190,7 @@ class CslcCmrQuery(CmrQuery):
         which granules to download. And also retrieve k granules.
         In reprocessing, just retrieve the k granules.
 
-        :param granules: 
+        :param granules:
 
         """
 
@@ -365,9 +365,9 @@ since the first CSLC file for the batch was ingested which is greater than the g
     ):
         """# Go back as many 12-day windows as needed to find k- granules that have at least the same bursts as the current frame
 
-        :param downloads: 
-        :param args: 
-        :param k_minus_one: 
+        :param downloads:
+        :param args:
+        :param k_minus_one:
         :param VV_only:  (Default value = True)
         :param silent:  (Default value = False)
 
@@ -457,12 +457,12 @@ since the first CSLC file for the batch was ingested which is greater than the g
     def query_cmr_by_native_id(self, args, token, cmr, settings, now, native_id):
         """
 
-        :param args: 
-        :param token: 
-        :param cmr: 
-        :param settings: 
-        :param now: 
-        :param native_id: 
+        :param args:
+        :param token:
+        :param cmr:
+        :param settings:
+        :param now:
+        :param native_id:
 
         """
 
@@ -521,12 +521,12 @@ since the first CSLC file for the batch was ingested which is greater than the g
     ):
         """
 
-        :param args: 
-        :param token: 
-        :param cmr: 
-        :param settings: 
-        :param now: 
-        :param timerange: 
+        :param args:
+        :param token:
+        :param cmr:
+        :param settings:
+        :param now:
+        :param timerange:
         :param silent:  (Default value = False)
 
         """
@@ -557,12 +557,12 @@ since the first CSLC file for the batch was ingested which is greater than the g
     def query_cmr(self, args, token, cmr, settings, timerange, now):
         """
 
-        :param args: 
-        :param token: 
-        :param cmr: 
-        :param settings: 
-        :param timerange: 
-        :param now: 
+        :param args:
+        :param token:
+        :param cmr:
+        :param settings:
+        :param timerange:
+        :param now:
 
         """
 
@@ -650,8 +650,8 @@ since the first CSLC file for the batch was ingested which is greater than the g
     def create_download_job_params(self, query_timerange, chunk_batch_ids):
         """Same as base class except inject batch_ids for k granules
 
-        :param query_timerange: 
-        :param chunk_batch_ids: 
+        :param query_timerange:
+        :param chunk_batch_ids:
 
         """
 
@@ -662,11 +662,11 @@ since the first CSLC file for the batch was ingested which is greater than the g
     def eliminate_duplicate_granules(self, granules):
         """For CSLC granules revision_id is always one. Instead, we correlate the granules by the unique_id
         which is a function of download_batch_id and burst_id
-        
+
         You must run extend_additional_records before calling this function because it requires granules to have
         many properties that are added by that function.
 
-        :param granules: 
+        :param granules:
 
         """
 
@@ -690,7 +690,7 @@ since the first CSLC file for the batch was ingested which is greater than the g
     def eliminate_none_frames(self, granules):
         """Get rid of frames that don't show up in the historical database json.
 
-        :param granules: 
+        :param granules:
 
         """
 
@@ -705,7 +705,7 @@ since the first CSLC file for the batch was ingested which is greater than the g
     def get_download_chunks(self, batch_id_to_urls_map):
         """For CSLC chunks we must group them by the batch_id that were determined at the time of triggering
 
-        :param batch_id_to_urls_map: 
+        :param batch_id_to_urls_map:
 
         """
 
